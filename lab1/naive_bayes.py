8ef27b2bfdae876ced7d0927f3d63a688f74b4e1
Macbook <carlos.valdez@kuka.com>
2019-02-06 16:36:29 +0100

    added posterior beginning

--- lab1/naive_bayes.py
+++ lab1/naive_bayes.py
@@ -25,7 +25,16 @@ Things to keep track of:
             - Calculation for P(word_1, word_2,...,word_i|1.0) = (word_scorings[0][word_1]/score_freqs[0]) * (word_scorings[0][word_2]/score_freqs[0]) *...* (word_scorings[0][word_i]/score_freqs[0])
 """
 
-
+# each dictionary maps a word to the # of time
+one_stars = dict()
+two_stars = dict()
+three_stars = dict()
+four_stars = dict()
+five_stars = dict()
+ratings = [one_stars, two_stars, three_stars, four_stars, five_stars]
+num_words = 0
+score_freqs = [0, 0, 0, 0, 0]
+score_probs = [0.0, 0.0, 0.0, 0.0, 0.0]
 # Enumerate through every line of the file and split into bag of words
 # TODO(?): Modularize this to accept any file
 for _ ,line in enumerate(train_set):
@@ -41,6 +50,54 @@ for _ ,line in enumerate(train_set):
     review = review.translate(review.maketrans('','',string.punctuation))
     #lowercase
     review = review.lower()
-    filter(None, review.split(" "))
-    print(review)
-    #words = filter(None, string.lower(review.translate(string.punctuation)).split(" "))
+    review = filter(None, review.split(" "))
+    # print(review)
+    _rating_index = int(score - 1)
+
+    for word in review:
+        if word in ratings[_rating_index]:
+            ratings[_rating_index][word] += 1
+        else:
+            ratings[_rating_index][word] = 1
+        num_words += 1
+        score_freqs[_rating_index] += 1
+
+        #words = filter(None, string.lower(review.translate(string.punctuation)).split(" "))
+
+for num in range(0, 5):
+    score_probs[num] = float(score_freqs[num]) / float(num_words)
+
+print(ratings)
+print("number of words", num_words)
+print(score_freqs)
+print(score_probs)
+
+# now clean up our test_set -----
+for _ ,line in enumerate(test_set):
+    review = line[line.find(',')+1:line.rfind(',')]
+    score = float(line[line.rfind(', ')+1:].rstrip("\n"))
+    # Can fine-tune the trimming of punctutation later if accuracy is to low
+    # These are just preliminary filters
+    review = review.replace('<br />', " ")
+    review = review.replace('.', " ")
+    review = review.replace('-', " ")
+    # Remove punctuation, lowercase entire string, split with the (" ") delimiter, remove empty strings
+    #remove punctuation
+    review = review.translate(review.maketrans('','',string.punctuation))
+    #lowercase
+    review = review.lower()
+    review = filter(None, review.split(" "))
+    # print(review)
+    _rating_index = int(score - 1)
+
+result = 0.0
+posteriors =  [0.0, 0.0, 0.0, 0.0, 0.0]
+for rating in range(1, 6):
+    for ithword in review:
+        # frequency = total number of times a word has been rated a score (1-5)
+        # divide frequency by the number of times THAT rating has been given to any word
+        # amount of times this word has been rated 1.000 - 5.00 divided by the total amount it's been  rated in all 
+
+likelyhood for 1/ score frequencies 
+
+
