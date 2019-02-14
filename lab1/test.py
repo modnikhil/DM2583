da752e7309312a3bf6fda5a2dad38233391e2f0f
Macbook <carlos.valdez@kuka.com>
2019-02-14 16:12:30 +0100

    deleted redundant files

--- lab1/test.py
+++
@@ -1,9 +0,0 @@
-
-words = ["hello", "hi", "what", "only"]
-test = dict([(word, True)  for word in words])
-# test_2 = list(map(test, words))
-
-print(str(test))
-
-
-# print("test_2: " + str(test_2))
