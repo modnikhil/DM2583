1beb44ec6861cad591352f3dbcab8562d3e9a5e8
modnikhil <mod.nikhil@gmail.com>
2019-02-18 12:56:04 +0100

    edits

--- lab 2/svm.py
+++ lab 2/svm.py
@@ -3,6 +3,11 @@ import string
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
+import sklearn.svm as svm
+import matplotlib.pyplot as plt
+import itertools
+from sklearn.metrics import confusion_matrix
+from sklearn import datasets
 
 # sources: https://github.com/iolucas/nlpython/blob/master/blog/sentiment-analysis-analysis/svm.ipynb
 # https://medium.com/nlpython/sentiment-analysis-analysis-part-2-support-vector-machines-31f78baeee09
@@ -33,19 +38,20 @@ def parse_files(file_set, labels_list):
             review = review.translate(review.maketrans('','',string.punctuation))
 
             review = review.lower()
-
+            review = " ".join(filter(None, review.split()))
+            #print(review)
             file_list.append(review)
             if score <= 3:
                 labels_list.append('neg ')
             else:
                 labels_list.append('pos ')
         count += 1
-    return file_list
+    return [indiv_review.split() for indiv_review in file_list]
 
 train_labels = []
-test_labels = []
-review_list = parse_files(train_set, train_labels)
-test_list = parse_files(test_set, test_labels)
+review_tokens = parse_files(train_set, train_labels)
+#print(review_list[0])
+#print(review_list[0])
 
 # -- for testing, prints the reviews and whether their respective class/label --
 # curr = 0
@@ -57,10 +63,14 @@ test_list = parse_files(test_set, test_labels)
 
 # load the module to transform review inputs into binary vectors using MulriLabelBinarizer class
 onehot_enc = MultiLabelBinarizer()
-onehot_enc.fit(review_list)
+onehot_enc.fit(review_tokens)
+
 
 # split data into training and test set with train_test_split function
-x_train, x_test, y_train, y_test = train_test_split(review_list, train_labels, test_size=100, random_state=None)
+x_train, x_test, y_train, y_test = train_test_split(review_tokens, train_labels, test_size=100, random_state=1234, shuffle=False)
+
+
+
 
 # create svm classifier and train it
 lsvm = LinearSVC()
@@ -70,3 +80,65 @@ lsvm.fit(onehot_enc.transform(x_train), y_train)
 score = lsvm.score(onehot_enc.transform(x_test), y_test)
 
 print("SVM Classifier score: the classifier performed on the test set with an accuracy of " + str(score * 100) + " %")
+
+
+#score = lsvm.score(onehot_enc.transform(x_test), y_test)
+#print("SVM Classifier score: the classifier performed on the test set with an accuracy of " + str(score * 100) + " %")
+#
+
+y_pred = lsvm.predict(onehot_enc.transform(x_test))
+print(y_pred)
+
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+
+print(cnf_matrix)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    #This function prints and plots the confusion matrix.
+    #Normalization can be applied by setting `normalize=True`.
+    """ 
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+    np.set_printoptions(precision=2)
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=["neg", "pos"], normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
+
+
+
+
